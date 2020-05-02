064a9183f05c2aa07577b02c5d61f2ae34486745
Deepak Deedar Singla <47693198+deepakdeedar@users.noreply.github.com>
2020-05-02 15:18:57 +0530

    New Features Added

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -18,6 +18,26 @@ class CartItem extends StatelessWidget {
     return Dismissible(
       key: ValueKey(id),
       direction: DismissDirection.endToStart,
+      confirmDismiss: (direction){
+        return showDialog(context: context,builder: (ctx) =>
+            AlertDialog(title: Text('Are you sure?',),
+              content: Text('Do you want to remove the item from the cart?'),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text('No'),
+                  onPressed: (){
+                    Navigator.of(ctx).pop(false);
+                  },
+                ),
+                FlatButton(
+                  child: Text('Yes'),
+                  onPressed: (){
+                    Navigator.of(ctx).pop(true);
+                  },
+                )
+              ],
+            ));
+      },
       background: Container(
         color: Colors.red,
         child: Icon(Icons.delete,color: Colors.white,size: 40,),
